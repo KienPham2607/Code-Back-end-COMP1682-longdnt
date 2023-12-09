c7facc73c8a418286d553c366c84170458654e1b
KienPham2607 <kienptgch200815@fpt.edu.vn>
2023-12-09 13:46:03 +0700

    done day 1

--- Lesson1/routes/categories.js
+++ Lesson1/routes/categories.js
@@ -8,7 +8,39 @@ router.get('/', async (req, res) => {
     // Get data from collection
     var categoryList = await CategoryModel.find({});
     // load data
-    res.send(categoryList); // Demo
+    // res.send(categoryList); // Demo
+    res.render("category/index", {categoryList});
 });
+
+router.get('/delete/:id', async (req, res) => {
+    // Get value from URL: req.params
+    var id = req.params.id;
+    await CategoryModel.findByIdAndDelete(id);
+    res.redirect("/categories");
+})
+
+router.get("/add", (req, res) => {
+    res.render("category/add");
+})
+
+router.post("/add", async (req, res) => {
+    // Get value from form: req.body
+    var category = req.body;
+    await CategoryModel.create(category);
+    res.redirect("/categories");
+})
+
+router.get("/edit:id", async (req, res) => {
+    var id = req.params.id;
+    var category = await CategoryModel.findById(id);
+    res.render("category/edit", {category});
+})
+
+router.post("/edit", async (req, res) => {
+    // Get value from form: req.body
+    var category = req.body;
+    await CategoryModel.create(category);
+    res.redirect("/categories");
+})
 
 module.exports = router;
--- Lesson1/routes/products.js
+++ Lesson1/routes/products.js
@@ -1,15 +1,41 @@
 var express = require("express");
 var router = express.Router();
 var ProductModel = require('../models/ProductModel');
+var CategoryModel = require('../models/CategoryModel');
+
 // Show all categories
 // URL: localhost:3000/categories
 // SQL: SELECT * FROM categories
 router.get('/', async (req, res) => {
     // Get data from collection
-    var productList = await ProductModel.find({});
-    // load data
+    var productList = await ProductModel.find({}).populate('category');
+    // load data    
     // res.send(productList); // Demo
     res.render("product/index", {productList}); // File location: view/product/index
 });
+
+router.get('/add', async (req, res) => {
+    var categoryList = await CategoryModel.find({});
+    res.render('product/add', { categoryList});
+})
+
+router.post('/add', async (req, res) => {
+    var product = req.body;
+    await ProductModel.create(product);
+    res.redirect('/products');
+})
+
+router.get('/edit/:id', async (req, res) => {
+    var id = req.params.id;
+    var product = await ProductModel.findById(id);
+    res.render('product/edit', { product });
+})
+
+router.post('/edit/:id', async (req, res) => {
+    var id = req.params.id;
+    var data = req.body;
+    await ProductModel.findByIdAndUpdate(id, data);
+    res.redirect('/products');
+})
 
 module.exports = router;
